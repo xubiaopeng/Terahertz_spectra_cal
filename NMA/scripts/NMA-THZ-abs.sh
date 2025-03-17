4a5f0de00d2856fffb98caeac9af439d51586e07
xubiaopeng <xubiaopeng@bit.edu.cn>
2025-03-17 21:14:42 +0800

    Update NMA-THZ-abs.sh

--- NMA/scripts/NMA-THZ-abs.sh
+++ NMA/scripts/NMA-THZ-abs.sh
@@ -1,20 +1,22 @@
-source /home/xubiao/gromacs_201806/bin/GMXRC
-gmx_d make_ndx -f file.pdb -o index.ndx
-gmx_d editconf -f file.pdb -o file_ha.pdb -n index.ndx
-gmx_d pdb2gmx -f file.pdb -ter -merge all -ingh -missing
-gmx_d grompp -v -f cg.mdp -c conf.gro -p topol.top -o protein-vacuum.tpr
-gmx_d mdrun -v -deffnm protein-vacuum -c protein-vacuum.gro -s protein-vacuum.tpr
+cd ../example;
 
-source /usr/local/gromacs/bin/GMXRC
-grompp_d -v -f cg.mdp -c conf.gro -p topol.top -o protein-vacuum_ref455.tpr -maxwarn 100
-trjconv_d -f protein-vacuum.trr -s protein-vacuum_ref455.tpr  -ndec 24 -o test.gro -dump 28776
-source /home/xubiao/gromacs_201806/bin/GMXRC
-gmx_d grompp -v -f nm.mdp -c test.gro -p topol.top -o protein-vacuum-nma.tpr
-gmx_d mdrun -v -s protein-vacuum-nma.tpr -mtx protein-vacumm-nma.mtx
-gmx_d nmeig -f protein-vacumm-nma.mtx -s protein-vacuum-nma -first 1 -last 400
+source /home/xubiao/gromacs_201806/bin/GMXRC #load double precision gromacs2018
+gmx_d pdb2gmx -f file.pdb -ter -merge all -ingh -missing # generate .top file
+gmx_d grompp -v -f cg.mdp -c conf.gro -p topol.top -o protein-vacuum.tpr # generate .tpr file for energy minimization
+gmx_d mdrun -v -deffnm protein-vacuum -c protein-vacuum.gro -s protein-vacuum.tpr # run double precision energy minimization
 
-jupyter notebook --no-browser --ip=0.0.0.0
+source /usr/local/gromacs/bin/GMXRC # load gromacs version 4.5.5 to generate gro files with high precision (24 decimals) from protein-vacuum.trr 
+grompp_d -v -f cg.mdp -c conf.gro -p topol.top -o protein-vacuum_ref455.tpr -maxwarn 100 # generate the tpr file for gromacs version 4.5.5 
+trjconv_d -f protein-vacuum.trr -s protein-vacuum_ref455.tpr  -ndec 24 -o test.gro -dump 28776 #take the final frame of the trajectory from energy minization to generate the high precision gro file 
+source /home/xubiao/gromacs_201806/bin/GMXRC #load double precision gromacs2018
+gmx_d grompp -v -f nm.mdp -c test.gro -p topol.top -o protein-vacuum-nma.tpr # use the high precision EM gro file to generate the tpr file for normal mode (NM) analysis
+gmx_d mdrun -v -s protein-vacuum-nma.tpr -mtx protein-vacumm-nma.mtx # if the precision of protein-vacuum-nma.tpr is not high enough, the graidient will not equal to zero, and the NM will fail with negative eigenvalues. 
+gmx_d nmeig -f protein-vacumm-nma.mtx -s protein-vacuum-nma -first 1 -last 400 
 
+cd -;
+#jupyter notebook --no-browser --ip=0.0.0.0 #
+jupyter notebook calculate_intensity_from_charge_theory_and_example.py
 
+cd ../example;
 gmx_d nmtraj -s test.gro -v eigenvec.trr -o NMA-file.pdb -eignr 12 -phases 0 -temp 20000
 gmx_d rmsf -f NMA-file.pdb -s conf.gro -o rmsf-file.xvg -res
